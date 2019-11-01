0fa7d3f0e8934e23afddee4a3b600a7d90964323
arthit75420 <arthit.n@ku.th>
2019-11-01 21:08:09 +0700

    share

---
+++ app.py
@@ -0,0 +1,227 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# import argparse
+# import sys
+import os
+import shutil
+import time
+import uuid
+import atexit
+
+import numpy as np
+import tensorflow as tf
+# import io
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask_cors import CORS
+# , jsonify, send_from_directory
+from flask import Flask, request, render_template, json
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+app.secret_key = "super secret key"
+CORS(app)
+
+
+def clear_id():
+    folder = 'tf/tf_files/uploads/user-images/'
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
+    return True
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=clear_id, trigger="interval", hours=1)
+scheduler.start()
+atexit.register(lambda: scheduler.shutdown())
+
+partLeaf = ["basil_leaf", "basil_leaf_notperfect", "chili_leaf", "chili_leaf_notperfect", "kaffir_lime_leaf",
+            "kaffir_lime_leaf_notperfect", "lemon_basil_leaf", "lemon_basil_leaf_notperfect", "lemon_leaf",
+            "lemon_leaf_notperfect", "mint_leaf", "mint_leaf_notperfect", "sweet_basil_leaf",
+            "sweet_basil_leaf_notperfect"]
+
+
+@app.route("/", methods=["GET"])
+def main():
+    return render_template('index.html')
+
+
+@app.route("/predict", methods=["POST"])
+def prepare():
+    file = request.files['file']
+    client_id = request.form.get("id")
+    res = {}
+    if(client_id == ""):
+        res["status"] = "error"
+        res["response"] = "id not found!"
+    else:
+        locate_file_path = "tf/tf_files/uploads/user-images/{}".format(
+            client_id)
+        if not os.path.exists(locate_file_path):
+            os.makedirs(locate_file_path)
+        res["status"] = "success"
+        res["response"] = preprocessing(file, locate_file_path, client_id)
+    return json.dumps(res)
+
+
+@app.route("/save", methods=["POST"])
+def save_img():
+    leaf_path = request.form.get("leaf")
+    client_id = request.form.get("id")
+    res = {}
+    if(client_id == ""):
+        res["status"] = "error"
+        res["response"] = "id not found!"
+    else:
+        full_path_id = "tf/tf_files/uploads/user-images/{}".format(client_id)
+        full_path_leaf = "tf/tf_files/uploads/dataset/{}".format(leaf_path)
+        if not os.path.exists(full_path_id):
+            return "error id not found!"
+        if not os.path.exists(full_path_leaf):
+            os.makedirs(full_path_leaf)
+        randomString = uuid.uuid4().hex
+        randomString = randomString.lower()[0:10]
+        shutil.move("{}/leaf.png".format(full_path_id),
+                    "{}/{}.png".format(full_path_leaf, randomString))
+        res["status"] = "success"
+        res["response"] = "Saved"
+    return json.dumps(res)
+
+
+def preprocessing(file, locate_file_path, client_id):
+    full_path_file_name = "{}/leaf.png".format(locate_file_path)
+    file.save(full_path_file_name)  # saving uploaded img
+    graph = load_graph(model_file)
+    t = read_tensor_from_image_file(full_path_file_name,
+                                    input_height=input_height,
+                                    input_width=input_width,
+                                    input_mean=input_mean,
+                                    input_std=input_std)
+
+    input_name = "import/" + input_layer
+    output_name = "import/" + output_layer
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
+
+    with tf.compat.v1.Session(graph=graph) as sess:
+        start = time.time()
+        results = sess.run(output_operation.outputs[0], {
+                           input_operation.outputs[0]: t})
+        end = time.time()
+    results = np.squeeze(results)
+    top_k = results.argsort()[-5:][::-1]
+    labels = load_labels(label_file, True)
+
+    Return = {}
+    Return["time"] = '{:.3f}'.format(end-start)
+    Return["results"] = []
+    Return["leafs"] = []
+    print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
+    template = "{} (score={:0.5f})"
+    leafPerfect = ["สมบูรณ์", "ไม่สมบูรณ์"]
+    leafName = {
+        "basil_leaf": ["กระเพรา", 0],
+        "basil_leaf_notperfect": ["กระเพรา", 1],
+        "chili_leaf": ["พริก", 0],
+        "chili_leaf_notperfect": ["พริก", 1],
+        "kaffir_lime_leaf": ["มะกรูด", 0],
+        "kaffir_lime_leaf_notperfect": ["มะกรูด", 1],
+        "lemon_basil_leaf": ["แมงลัก", 0],
+        "lemon_basil_leaf_notperfect": ["แมงลัก", 1],
+        "lemon_leaf": ["มะนาว", 0],
+        "lemon_leaf_notperfect": ["มะนาว", 1],
+        "mint_leaf": ["สะระแหน่", 0],
+        "mint_leaf_notperfect": ["สะระแหน่", 1],
+        "sweet_basil_leaf": ["โหระพา", 0],
+        "sweet_basil_leaf_notperfect": ["โหระพา", 1]
+    }
+    for i in top_k:
+        result = {}
+        result["leaf"] = '{}'.format(labels[i])
+        result["leafThai"] = leafName['{}'.format(labels[i])][0]
+        result["perfect"] = leafPerfect[leafName['{}'.format(labels[i])][1]]
+        result["percent"] = '{:0.2f}'.format(results[i]*100)
+        Return["results"].append(result)
+        print(template.format(labels[i], results[i]))
+    for i in labels:
+        result = {}
+        result["leaf"] = i
+        result["leafThai"] = 'ใบ{}{}'.format(
+            leafName[i][0], leafPerfect[leafName[i][1]])
+        Return["leafs"].append(result)
+    return Return
+
+
+def load_graph(model_file):
+    graph = tf.compat.v1.Graph()
+    graph_def = tf.compat.v1.GraphDef()
+
+    with open(model_file, "rb") as f:
+        graph_def.ParseFromString(f.read())
+    with graph.as_default():
+        tf.import_graph_def(graph_def)
+
+    return graph
+
+
+def read_tensor_from_image_file(file_name, input_height=224, input_width=224, input_mean=0, input_std=255):
+    input_name = "file_reader"
+    # output_name = "normalized"
+    file_reader = tf.compat.v1.read_file(file_name, input_name)
+    if file_name.endswith(".png"):
+        image_reader = tf.image.decode_png(
+            file_reader, channels=3, name='png_reader')
+    elif file_name.endswith(".gif"):
+        image_reader = tf.squeeze(
+            tf.image.decode_gif(file_reader, name='gif_reader'))
+    elif file_name.endswith(".bmp"):
+        image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+    else:
+        image_reader = tf.image.decode_jpeg(
+            file_reader, channels=3, name='jpeg_reader')
+    float_caster = tf.cast(image_reader, tf.float32)
+    dims_expander = tf.expand_dims(float_caster, 0)
+    resized = tf.compat.v1.image.resize_bilinear(
+        dims_expander, [input_height, input_width])
+    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+    sess = tf.compat.v1.Session()
+    result = sess.run(normalized)
+    return result
+
+
+def load_labels(label_file, path=False):
+    label = []
+    proto_as_ascii_lines = tf.compat.v1.gfile.GFile(label_file).readlines()
+    for l in proto_as_ascii_lines:
+        if(path):
+            label.append(l.rstrip().replace(" ", "_"))
+        else:
+            label.append(l.rstrip())
+    return label
+
+
+if __name__ == "__main__":
+    file_name = "tf/tf_files/uploads/leaf.png"
+    model_file = "tf/tf_files/retrained_graph.pb"
+    label_file = "tf/tf_files/retrained_labels.txt"
+    input_height = 224
+    input_width = 224
+    input_mean = 128
+    input_std = 128
+    # Mull , input , Placeholder
+    input_layer = "input"
+    output_layer = "final_result"
+    # Flask init
+    app.debug = True
+    app.run(host='0.0.0.0', port=88)
---
+++ templates/js/index.js
@@ -0,0 +1,286 @@
+/**
+ * . => class
+ * # => id
+ */
+var cropper;
+
+/**
+ * Cookie
+ */
+
+class Cookie {
+    constructor(init) {
+        this.init = init;
+    }
+    setCookie(cname, cvalue, exdays) {
+        let d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        return true;
+    }
+    getCookie(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+    checkCookie(cname) {
+        let cookieName = this.getCookie(cname);
+        if (cookieName != "") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/**
+ * Dom Event
+ */
+
+window.addEventListener('DOMContentLoaded', () => {
+    let cookie = new Cookie();
+    if (!cookie.checkCookie("id")) cookie.setCookie("id", Math.random().toString(36).substr(2, 10), 1);
+    sessionStorage.setItem("TopOne", "leaf");
+    let $selectLeaf = $("#leafs");
+    let $alert = $('#results-predict');
+    let $boxSaveImg = $('#box-save-img');
+    $alert.hide();
+    $("#accurate").click(() => {
+        $selectLeaf.empty();
+        try {
+            let leafs = JSON.parse(sessionStorage.getItem("leafs"));
+            let topOneLeaf = sessionStorage.getItem("TopOne");
+            $.each(leafs, (idx, obj) => {
+                if (obj.leaf == topOneLeaf) {
+                    $selectLeaf.append(`<option value="${obj.leaf}">${obj.leafThai}</option>`);
+                    return false;
+                }
+            });
+            $selectLeaf.prop('disabled', true);
+        } catch (e) {}
+    });
+
+    $("#inaccurate").click(() => {
+        $selectLeaf.empty();
+        try {
+            let leafs = JSON.parse(sessionStorage.getItem("leafs"));
+            $selectLeaf.prop('disabled', false);
+            $selectLeaf.append(`<option value="0" selected hidden>กรุณาเลือกใบ</option>`);
+            $.each(leafs, (idx, obj) => {
+                $selectLeaf.append(`<option value="${obj.leaf}">${obj.leafThai}</option>`);
+            });
+        } catch (e) {}
+    });
+    let $btLeafSave = $("#leaf-save");
+    $btLeafSave.click(() => {
+        let leaf = $("#leafs").val();
+        let id = cookie.getCookie("id");
+        if ($("#leafs").val() != "0") {
+            $.ajax({
+                url: "save",
+                method: "POST",
+                dataType: "json",
+                data: {
+                    leaf,
+                    id
+                }
+            }).done((res) => {
+                if (res.status == "success") {
+                    Swal.fire({
+                        type: 'success',
+                        title: 'บันทึกภาพเรียบร้อยแล้ว',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    $boxSaveImg.empty().html(`<div class="alert alert-success">
+                                                    <strong>บันทึกภาพเรียบร้อยแล้ว!</strong>
+                                                    </div>`);
+                } else if (res.status == "error") {
+                    Swal.fire({
+                        type: 'error',
+                        title: res.response,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                } else {
+                    Swal.fire({
+                        type: 'error',
+                        title: 'System error!',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+
+            }).fail((res) => {
+                Swal.fire({
+                    type: 'error',
+                    title: 'บันทึกภาพล้มเหลว',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            });
+        } else {
+            $("#leafs").focus();
+        }
+    });
+    // $("#select-leaf").on('hide.bs.modal', () => {
+    //     $btLeafSave.removeAttr("data-dismiss");
+    // });
+    $("#select-leaf").on('show.bs.modal', () => {
+        if ($("#leafs").val() != "0") $btLeafSave.attr("data-dismiss", "modal");
+        else $btLeafSave.removeAttr("data-dismiss");
+    });
+
+    $("#leafs").change(() => {
+        if ($("#leafs").val() != "0") $btLeafSave.attr("data-dismiss", "modal");
+    });
+
+    var avatar = document.getElementById('avatar');
+    var image = document.getElementById('image');
+    var input = document.getElementById('input');
+    var predict = document.getElementById('predict');
+    var $results = $("#results");
+    var $progress = $('.progress');
+    var $progressBar = $('.progress-bar');
+
+    var $modal = $('#modal');
+    $('[data-toggle="tooltip"]').tooltip();
+
+    input.addEventListener('change', (e) => {
+        var files = e.target.files;
+        var done = (url) => {
+            input.value = '';
+            image.src = url;
+            $alert.hide();
+            $modal.modal('show');
+        };
+        var reader;
+        var file;
+        var url;
+
+        if (files && files.length > 0) {
+            file = files[0];
+
+            if (URL) {
+                done(URL.createObjectURL(file));
+            } else if (FileReader) {
+                reader = new FileReader();
+                reader.onload = (e) => {
+                    done(reader.result);
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+    });
+
+    predict.addEventListener('click', () => {
+        $(predict).hide();
+        $progressBar.width('0%').attr('aria-valuenow', '0').text('0%');
+        $progress.show();
+        $alert.removeClass('alert-success alert-warning');
+        canvas.toBlob((blob) => {
+            let id = cookie.getCookie("id");
+            var formData = new FormData();
+            formData.append('file', blob, 'leaf.png');
+            formData.append("id", id);
+            $.ajax({
+                url: "predict",
+                method: "POST",
+                dataType: "json",
+                data: formData,
+                processData: false,
+                contentType: false,
+                xhr: () => {
+                    var xhr = new XMLHttpRequest();
+
+                    xhr.upload.onprogress = (e) => {
+                        var percent = '0';
+                        var percentage = '0%';
+
+                        if (e.lengthComputable) {
+                            percent = Math.round((e.loaded / e.total) * 100);
+                            percentage = percent + '%';
+                            $progressBar.width(percentage).attr('aria-valuenow', percent).text(percentage);
+                        }
+                    };
+
+                    return xhr;
+                }
+            }).done((res) => {
+                if (res.status == "success") {
+                    $results.empty();
+                    if (sessionStorage.getItem("leafs") == null)
+                        sessionStorage.setItem("leafs", JSON.stringify(res.response.leafs));
+                    $.each(res.response.results, (_i, result) => {
+                        if (_i == 0) sessionStorage.setItem("TopOne", result.leaf);
+                        $results.append(`<tr>
+                                            <td>${result.leafThai}</td>
+                                            <td>${result.percent}%</td>
+                                            <td>${result.perfect}</td>
+                                        </tr>`);
+                    });
+                    $("#results-box").removeClass('d-none');
+                    $alert.show().addClass('alert-success');
+                } else if (res.status == "error") {
+                    $("#results-box").addClass('d-none');
+                    $alert.show().addClass('alert-warning').text(res.response);
+                } else {
+                    $("#results-box").addClass('d-none');
+                    $alert.show().addClass('alert-danger').text("System error!");
+                }
+            }).fail(() => {
+                avatar.src = initialAvatarURL;
+                $("#results-box").addClass('d-none');
+                $alert.show().addClass('alert-warning').text('Upload error');
+            }).always(() => {
+                $progress.hide();
+            });
+        });
+    });
+
+    $modal.on('shown.bs.modal', () => {
+        cropper = new Cropper(image, {
+            dragMode: 'move',
+            aspectRatio: 3 / 4,
+            autoCropArea: 0.85,
+            restore: false,
+            guides: false,
+            center: false,
+            highlight: false,
+            cropBoxMovable: false,
+            cropBoxResizable: false,
+            toggleDragModeOnDblclick: false,
+            rotatable: true
+        });
+    }).on('hidden.bs.modal', () => {
+        cropper.destroy();
+        cropper = null;
+    });
+    var initialAvatarURL;
+    var canvas;
+    document.getElementById('crop').addEventListener('click', () => {
+        if (cropper) {
+            $(predict).show();
+            canvas = cropper.getCroppedCanvas({
+                width: 768,
+                height: 1024,
+            });
+            initialAvatarURL = avatar.src;
+            avatar.src = canvas.toDataURL();
+            $("#leafShow").attr("src", canvas.toDataURL());
+            $boxSaveImg.empty().html(`<button type="button" class="btn btn-primary" 
+                                        data-toggle="modal" data-target="#report" 
+                                        id="bt-save">บันทึกภาพ</button>`);
+        }
+    });
+});
